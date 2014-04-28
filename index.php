cdc9f40882cc0e9bfe99176388b6f2cc73253cfd
Blendi Gashi <blendi.gashi@gmail.com>
2014-04-29 00:09:13 +0200

    Update

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Universiteti i Prizrenit</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stili.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	  	<header>
+	  	</header>	 
+	  	<div class="container">
+    	  <div class="navbar navbar-default" role="navigation">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	        </div>
+	        <div class="collapse navbar-collapse">
+	          <ul class="nav navbar-nav">
+	          <li class="logo"><img src="http://uni-prizren.com/inc/images/main-logo.png"/></li>
+	            <li class="active"><a href="index.php">Ballina</a></li>
+	            <li><a href="#about">P&euml;r universitetin</a></li>
+	            <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Fakultetet <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Fakulteti i shkencave kompjuterike</a></li>
+	                <li><a href="#">Fakulteti ekonomik</a></li>
+	                <li><a href="#">Fakulteti i edukimit</a></li>
+	                <li><a href="#">Fakulteti juridik</a></li>
+	                <li class="divider"></li>
+	                <li class="dropdown-header">Departamentet</li>
+	                <li><a href="#">N&euml; gjuh&euml;n shqipe</a></li>
+	                <li><a href="#">N&euml; gjuh&euml;n turke</a></li>
+	                <li><a href="#">N&euml; gjuh&euml;n boshnjake</a></li>
+	              </ul>
+	            </li>
+	            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Orari i provimeve <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Janar</a></li>
+	                <li><a href="#">Prill</a></li>
+	                <li><a href="#">Qershor</a></li>
+	                <li><a href="#">Shtator</a></li>
+	                <li><a href="#">N&euml;ntor</a></li>
+	              </ul>
+	            </li>
+	            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Orari i ligj&euml;ratave <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Fakulteti i shkencave kompjuterike</a></li>
+	                <li><a href="#">Fakulteti ekonomik</a></li>
+	                <li><a href="#">Fakulteti i edukimit</a></li>
+	                <li><a href="#">Fakulteti juridik</a></li>
+	              </ul>
+	            </li>
+	            <li><a href="#">Njoftimet</a></li>
+	            <li><a href="#">Projektet tempus</a></li>
+	            <li><a href="#">Konferenca</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	     </div>
+	    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
