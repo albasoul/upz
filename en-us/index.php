8996eb35745d2f4969cabdf0054dd9241d020529
arditmo <ar_diiti@live.com>
2014-04-29 03:58:15 +0200

    tani ne 2 gjuhe, shqip dhe anglisht

---
+++ en-us/index.php
@@ -0,0 +1,205 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>University of Prizren </title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stili.css" rel="stylesheet">
+	<link rel="icon" type="image/png" href="images/upz-favicon.png" />
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	  	<header>
+	  	</header>	 
+	  	<div class="container">
+	  	<div class="hidden-lg text-center"><img src="http://uni-prizren.com/inc/images/main-logo.png"/></div>
+    	  <div class="navbar navbar-default" role="navigation">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	        </div>
+	        <div class="collapse navbar-collapse">
+	          <ul class="nav navbar-nav">
+	          <li class="logo hidden-xs hidden-sm hidden-md"><img src="http://uni-prizren.com/inc/images/main-logo.png"/></li>
+	            <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+	            <li><a href="#about">About the University</a></li>
+	            <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Faculties<b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Faculty of Computer Science</a></li>
+	                <li><a href="#">Faculty of Economics</a></li>
+	                <li><a href="#">Faculty of Education</a></li>
+	                <li><a href="#">Faculty of Law</a></li>
+	                <li class="divider"></li>
+	                <li class="dropdown-header">Departments</li>
+	                <li><a href="#">In Albanian language</a></li>
+	                <li><a href="#">In Turkish language</a></li>
+	                <li><a href="#">In Bosnian Language</a></li>
+	              </ul>
+	            </li>
+	            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Exams Timetable<b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">January</a></li>
+	                <li><a href="#">April</a></li>
+	                <li><a href="#">June</a></li>
+	                <li><a href="#">September</a></li>
+	                <li><a href="#">November</a></li>
+	              </ul>
+	            </li>
+	            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Schedule of Lectures<b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Faculty of Computer Science</a></li>
+	                <li><a href="#">Faculty of Economics</a></li>
+	                <li><a href="#">Faculty of Education</a></li>
+	                <li><a href="#">Faculty of Law</a></li>
+	              </ul>
+	            </li>
+	            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">News<b class="caret"></b></a>
+	            	<ul class="dropdown-menu">
+	                <li><a href="#">Admitted Students</a></li>
+	                <li><a href="#">Events</a></li>
+	              </ul>
+	            </li>
+	            <li><a href="#">Tempus Projects</a></li>
+	            <li><a href="#">Conferences</a></li>
+	          </ul>
+	          <ul class="nav navbar-nav navbar-right">
+	          	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-chevron-down"></span></a>
+	          		<ul class="dropdown-menu">
+	          		<li><a href="#">Competitions</a></li>
+	                <li><a href="#">Intercommunication</a></li>
+	                <li><a href="#">Guide GIZ-CDBE</a></li>
+	                <li><a href="#">Scientific Journals</a></li>
+	                <li><a href="#">Laws / Regulations</a></li>
+	                <li><a href="#">Contact</a></li>
+	              </ul>
+	             </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	     </div>
+	     <br/>
+	     <div class="col-md-12 panel panel-default">
+	     	<div class="row panel-body">
+		     	<div class="col-md-8">
+			     	<div id="upz-carousel" class="carousel slide" data-ride="carousel">
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner">
+					    <div class="item active">
+					      <img src="http://uni-prizren.com/repository/images/thumb_590x320/foto_nr_1_469494.jpg" alt="...">
+					    </div>
+					    <div class="item">
+					      <img src="http://uni-prizren.com/repository/images/thumb_590x320/Foto_nr_2_613670.jpg" alt="...">
+					    </div>
+					    <div class="item">
+					      <img src="http://uni-prizren.com/repository/images/thumb_590x320/Foto_nr_3_42400.jpg" alt="...">
+					    </div>
+					  </div>
+					  
+					</div>
+				</div>
+				<div class="col-md-4">
+					<p class="text-center"><img class="img-responsive img-thumbnail" src="http://rahovecpress.com/wp-content/uploads/2014/03/Shemsedini-300x189.jpg"/></p>
+					<p class="text-center lead"><strong>Prof.Dr.Zahadin Shemsedini</strong></p>
+					<p class="text-justify"> Të nderuar studentë, mirë se keni ardhur në Universitetin Publik të Prizrenit!</p>
+					<p class="small text-right"><a href="#">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="panel panel-default aktualitetet">
+					<div class="panel-heading">
+					    <h3 class="panel-title">Recent News</h3>
+					  </div>
+					<div class="panel-body">
+						<div class="col-md-3 aktualiteti">
+							<p class="small text-danger">12 April 2014</p>
+							<p class="lead">Notice</p>
+							<p class="text-justify">Notice     Të nderuar studiues, Të bashkangjitur mund ta gjeni programin e konferencës, e cila do t'i zhvillon aktivitetet shkencore në ambientin e Universitetit të Prizrenit, Prizren.</p>
+							<a class="btn btn-sm btn-default">Read more</a>
+						</div>
+						<div class="col-md-3 aktualiteti">
+							<p class="small text-danger">12 April 2014</p>
+							<p class="lead">Notice</p>
+							<p class="text-justify">Notice     Të nderuar studiues, Të bashkangjitur mund ta gjeni programin e konferencës, e cila do t'i zhvillon aktivitetet shkencore në ambientin e Universitetit të Prizrenit, Prizren.</p>
+							<a class="btn btn-sm btn-default">Read more</a>
+						</div>
+						<div class="col-md-3 aktualiteti">
+							<p class="small text-danger">12 April2014</p>
+							<p class="lead">Notice</p>
+							<p class="text-justify">Notice     Të nderuar studiues, Të bashkangjitur mund ta gjeni programin e konferencës, e cila do t'i zhvillon aktivitetet shkencore në ambientin e Universitetit të Prizrenit, Prizren.</p>
+							<a class="btn btn-sm btn-default">Read more</a>
+						</div>
+						<div class="col-md-3 aktualiteti">
+							<p class="small text-danger">12 April 2014</p>
+							<p class="lead">Notice</p>
+							<p class="text-justify">Notice     Të nderuar studiues, Të bashkangjitur mund ta gjeni programin e konferencës, e cila do t'i zhvillon aktivitetet shkencore në ambientin e Universitetit të Prizrenit, Prizren.</p>
+							<a class="btn btn-sm btn-default">Read more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+	     </div>
+	   </div>
+	   	<div id="footer">
+	     	<div class="container">
+	     		<div class="adresa col-md-3">
+	     			<p class="lead">Address</p>
+	     			<address>
+					  <strong>University "Ukshin Hoti"</strong><br>
+					  Rruga e Shkronjave, nr.1<br>
+					  Prizren, 20000<br>
+					  Republic of Kosovo<br>
+					  <abbr title="Telefoni">Tel:</abbr> (029) 232-140<br>
+					  e-mail: <a href="mailto:rektorati@uni-prizren.com">rektorati@uni-prizren.com</a>
+					</address>
+				</div>
+				<div class="col-md-3">
+					<ul class="list-unstyled">
+					  <li><p class="lead">Social Network</p></li>
+					  <li><a href="https://www.facebook.com/Uni.UkshiHoti">Connect with Facebook</a></li>
+					  <li><a href="">Connect with Twitter</a></li>
+					  <li><a href="">Youtube Channel</a></li>
+					  <li><a href="">GitHub Fork</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<ul class="list-unstyled">
+					  <li><p class="lead">Language</li>
+					  <li><a href="http://localhost/upz/sq/index.php">Shqip</a></li>
+					  <li><a href="http://localhost/upz/en-us/index.php">English</a></li>
+					  <li><a href="">Turkish</a></li>
+					  <li><a href="">Bosanski</a></li>
+					  <li><a href="">German</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<ul class="list-unstyled">
+					  <li><p class="lead">Powered By</p></li>
+					  <li><a href="">Ardit Morina</a></li>
+					  <li><a href="">Besfort Bajrami</a></li>
+					  <li><a href="">Lirim Gashi</a></li>
+					  <li><a href="">Blendi Gashi</a></li>
+					</ul>
+				</div>
+	     	</div>
+	    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
